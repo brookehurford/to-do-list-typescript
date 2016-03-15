65eae50a4c7c4fe9f9210f97793e0bff2cde3a72
Brooke Hurford and Oskar Radon <oskarradonkimball@gmail.com>
2016-03-15 09:12:19 -0700

    Add bower CSS and JS concat and minify tasks and make them a part of meta build task

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,23 @@
 
-var gulp = require('gulp');
+var gulp       = require('gulp');
 var browserify = require('browserify');
-var source = require('vinyl-source-stream');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var utilities = require('gulp-util');
-var del = require('del');
-var jshint = require('gulp-jshint');
+var source     = require('vinyl-source-stream');
+var concat     = require('gulp-concat');
+var uglify     = require('gulp-uglify');
+var utilities  = require('gulp-util');
+var del        = require('del');
+var jshint     = require('gulp-jshint');
+var lib        = require('bower-files')({
+  "overrides":{
+    "bootstrap" : {
+      "main": [
+        "less/bootstrap.less",
+        "dist/css/bootstrap.css",
+        "dist/js/bootstrap.js"
+      ]
+    }
+  }
+});
 
 var buildProduction = utilities.env.production;
 
@@ -16,7 +27,11 @@ gulp.task("build", ['clean'], function() {
   } else {
     gulp.start('jsBrowserify');
   }
+  gulp.start('bower');
 });
+
+gulp.task('bower', ['bowerJS', 'bowerCSS']);
+
 
 // gulp tasks
 
@@ -47,4 +62,17 @@ gulp.task('jshint', function() {
   return gulp.src(['js/*.js'])
     .pipe(jshint())
     .pipe(jshint.reporter('default'));
+});
+
+gulp.task('bowerJS', function () {
+  return gulp.src(lib.ext('js').files)
+    .pipe(concat('vendor.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./build/js'));
+});
+
+gulp.task('bowerCSS', function () {
+  return gulp.src(lib.ext('css').files)
+    .pipe(concat('vendor.css'))
+    .pipe(gulp.dest('./build/css'));
 });
