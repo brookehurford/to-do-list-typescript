74e812bce1222f7dd040c7cbf4fe16c24cfa66f9
Brooke Hurford and Oskar Radon <brookebhurford@gmail.com>
2016-03-15 15:52:04 -0700

    Add gulp start for CSSBuild on gulp production task

--- gulpfile.js
+++ gulpfile.js
@@ -31,6 +31,7 @@ gulp.task("build", ['clean'], function() {
     gulp.start('jsBrowserify');
   }
   gulp.start('bower');
+  gulp.start('CSSBuild');
 });
 
 gulp.task('bower', ['bowerJS', 'bowerCSS']);
